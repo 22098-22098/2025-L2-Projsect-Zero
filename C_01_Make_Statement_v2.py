2bbc1b5e4e282cda5b0da8c5713b75505fb67f89
22098-22098 <22098@fraser.school.nz>
2025-02-18 09:49:32 +1300

    Flexible multi-line statement generator component

---
+++ C_01_Make_Statement_v2.py
@@ -0,0 +1,22 @@
+def make_statement(statement, decloration, lines):
+
+
+    middle = (f"{decloration * 3} {statement} {decloration * 3}")
+    top_bottom = decloration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+make_statement("Programming is Fun!","=", 3)
+print()
+make_statement("s Still Fun!", "*", 2)
+print()
+make_statement("Emoji in Action", "🐍", "1")
---
+++ ZZ_Sandpit.py
@@ -0,0 +1,10 @@
+num = 5
+print(num * 3)
+
+num_string = "5"
+print(num_string * 3)
+
+ex_text = "Hello World"
+txt_length = len(ex_text)
+
+print(f"{ex_text} is {txt_length} characters long")
