dbf9627127f792aacc92f16d37fa14ee98241a64
22098-22098 <22098@fraser.school.nz>
2025-02-20 13:18:56 +1300

    20/2

---
+++ C_04_string_check.py
@@ -0,0 +1,3 @@
+def string_check(question, ans_list):
+    while True:
+        response = input(question).lower
---
+++ ZZ_Sandpit_v2.py
@@ -0,0 +1,11 @@
+fruit_list = ["apple", "banana", "cherry", "dragon fruit"]
+
+for i in fruit_list:
+
+    print()
+
+    print("Fruit Name: ", i)
+
+    print("First Letter", i[0])
+
+    print(f"First two letters {i[0]} {i[1]}")
